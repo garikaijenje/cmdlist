135cd4d7271a54bb8667b5a5d0d9a6e99874f3e3
Garikai Jenje <garikaijen@gmail.com>
2023-04-19 11:54:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# cmdlist
+
+cmdlist is a simple interactive CLI tool that lets you execute your frequently used terminal commands.
+
+## Usage
+
+1. Create a `.cmdlist` file inside your project directory.
+2. List all your commands in the following format: `alias::command`
+   - `alias` is the shortcut name for your command.
+   - `command` is the actual command that you want to execute.
+3. Run the `cmdlist` command from your project directory.
+4. Select the command you want to execute from the list.
+5. cmdlist will execute the selected command for you.
+
+## Installation
+
+To use cmdlist, you need to install it globally:
+
+`npm install -g cmdlist`
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+
+const readline = require("readline");
+const keypress = require("keypress");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+try {
+  const filename = path.join(__dirname, ".cmdlist");
+  const fileData = fs.readFileSync(filename, "utf8");
+  const commands = fileData.split("\n").map((line) => line.trim());
+
+  const options = commands.map((command) => command.split("::")[0]);
+
+  let selectedOptionIndex = 0;
+
+  const displayOptions = () => {
+    console.clear();
+    console.log("\nChoose an option:\n");
+    options.forEach((option, index) => {
+      if (index === selectedOptionIndex) {
+        console.log(`\x1b[36m> ${option}\x1b[0m`);
+      } else {
+        console.log(`  ${option}`);
+      }
+    });
+    console.log("\nUse arrow keys to navigate and press enter to select.");
+  };
+
+  displayOptions();
+
+  keypress(process.stdin);
+
+  process.stdin.on("keypress", (ch, key) => {
+    if (key && key.name === "up") {
+      selectedOptionIndex = Math.max(0, selectedOptionIndex - 1);
+      displayOptions();
+    } else if (key && key.name === "down") {
+      selectedOptionIndex = Math.min(
+        options.length - 1,
+        selectedOptionIndex + 1
+      );
+      displayOptions();
+    } else if (key && key.name === "return") {
+      console.clear();
+      const selectedOption = options[selectedOptionIndex];
+      const selectedCommand = commands[selectedOptionIndex].split("::")[1];
+      console.log(`\nYou selected: \x1b[36m${selectedOption}\x1b[0m\n`);
+      console.log(`Running command: \x1b[33m${selectedCommand}\x1b[0m\n`);
+      rl.close();
+      exec(selectedCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`\x1b[31mError:\x1b[0m ${error.message}`);
+        } else if (stderr) {
+          console.error(`\x1b[31mError:\x1b[0m ${stderr}`);
+        } else {
+          console.log(`\x1b[32mOutput:\x1b[0m ${stdout}`);
+        }
+      });
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+} catch (err) {
+  console.error(err);
+}
